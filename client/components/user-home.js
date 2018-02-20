0a3c4cc2549ef0118dcd10343d85ecb0e3a2be28
Kevin <omallek@gmail.com>
2018-02-20 11:24:15 -0600

    creates intro paragraph explaining plan for project

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -10,7 +10,10 @@ export const UserHome = (props) => {
 
   return (
     <div>
-      <h3>Welcome, {email}</h3>
+      <h3>Welcome to Challenge Tracker</h3>
+      <p> Challenge Tracker will be my attempt to research new technologies in order to create a visualization tracker.</p>
+      <p> I hope to use this app to keep track of my attemps at learning various whiteboad questions.  I also want to expand to keeping track of other 'challenges' such as fitness activity and public speaking. </p>
+      <p> As of this moment, it is my idea to research and use D3 for graph tracking and visualization, firebase and PostGresQL for data storage, and Semantic UI or Material UI for design.  I want to potentially research and use SASS or LESS for CSS and look into Grunt and Gulp, and compare against Webpack. </p>
     </div>
   )
 }
